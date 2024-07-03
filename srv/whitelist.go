553bcd9e76032eb14a9b97915994a3e614ff0119
Lanre Adelowo <adelowomailbox@gmail.com>
2024-07-03 08:51:08 +0100

    Update srv/whitelist.go
    Co-authored-by: Har Preet Singh <singhhp1069@gmail.com>

--- srv/whitelist.go
+++ srv/whitelist.go
@@ -15,28 +15,29 @@ func getWhitelistKey(addr common.Address) string {
 	return fmt.Sprintf("bundler-whitelist-%s", addr.Hex())
 }
 
-func CheckSenderWhitelist(db *badger.DB,
-	whitelistedAddress []common.Address,
-	logger logr.Logger) modules.BatchHandlerFunc {
-
-	logger.Info("setting addresses into badger for whitelisting checks")
-
-	if len(whitelistedAddress) > 0 {
-		logger.Info("Bundler address whitelisting is enabled",
-			"number_of_whitelisted_addresses", len(whitelistedAddress))
+func CheckSenderWhitelist(db *badger.DB, 
+whitelistedAddresses []common.Address, 
+logger logr.Logger) modules.BatchHandlerFunc {
+	logger.Info("Setting up addresses in BadgerDB for whitelist checks")
+	if len(whitelistedAddresses) > 0 {
+		logger.Info("Bundler address whitelisting enabled", "number_of_whitelisted_addresses", len(whitelistedAddresses))
 	}
 
-	for _, addr := range whitelistedAddress {
-		err := db.Update(func(txn *badger.Txn) error {
-			return txn.Set([]byte(getWhitelistKey(addr)), addr.Bytes())
-		})
-		if err != nil {
-			logger.Error(err, "could not store address in db for whitelisting",
-				"address", addr.Hex())
-			panic("could not store address in Badger for whitelisting checks")
+	// Use a single transaction to store all addresses
+	err := db.Update(func(txn *badger.Txn) error {
+		for _, addr := range whitelistedAddresses {
+			if err := txn.Set([]byte(getWhitelistKey(addr)), addr.Bytes()); err != nil {
+				logger.Error(err, "Failed to store address in DB for whitelisting", "address", addr.Hex())
+				return err
+			}
+			logger.Info("Address stored in DB whitelist", "address", addr.Hex())
 		}
+		return nil
+	})
 
-		logger.Info("address stored in cache whitelist", "address", addr.Hex())
+	if err != nil {
+		logger.Error(err, "Failed to store multiple addresses in BadgerDB")
+		return nil
 	}
 
 	return func(ctx *modules.BatchHandlerCtx) error {
@@ -45,25 +46,22 @@ func CheckSenderWhitelist(db *badger.DB,
 			Start(context.Background(), "WithAdressWhitelist")
 		defer span.End()
 
-		if len(whitelistedAddress) == 0 {
-			logger.Info("address whitelisting not enabled. Skipping middleware")
+		if len(whitelistedAddresses) == 0 {
+			logger.Info("Address whitelisting not enabled. Skipping middleware.")
 			return nil
 		}
 
 		for _, userop := range ctx.Batch {
-
+			senderKey := getWhitelistKey(userop.Sender)
 			err := db.View(func(txn *badger.Txn) error {
-				_, err := txn.Get([]byte(getWhitelistKey(userop.Sender)))
+				_, err := txn.Get([]byte(senderKey))
 				return err
 			})
-
 			if err != nil {
-				logger.Error(err, "sender not found in whitelist. cannot execute transaction",
-					"address", userop.Sender.Hex())
+				logger.Error(err, "Sender not found in whitelist; cannot execute transaction", "address", userop.Sender.Hex())
 				return err
 			}
 		}
-
 		return nil
 	}
 }
