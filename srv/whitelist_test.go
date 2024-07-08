c382bd45513ce792dfa2b26e750cecf2c2be8622
Lanre Adelowo <yo@lanre.wtf>
2024-07-08 12:24:12 +0100

    fix test

--- srv/whitelist_test.go
+++ srv/whitelist_test.go
@@ -1,7 +1,6 @@
 package srv
 
 import (
-	"log"
 	"testing"
 
 	"github.com/dgraph-io/badger/v3"
@@ -12,45 +11,128 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+//
+// func TestCheckSenderWhitelist(t *testing.T) {
+//
+// 	db, err := badger.Open(badger.DefaultOptions("testdata/test.db").WithInMemory(true))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	defer func() {
+// 		require.NoError(t, db.Close())
+// 	}()
+//
+// 	handler := CheckSenderWhitelist(db, []common.Address{
+// 		common.HexToAddress("0x000000000000000000000000000000000000dEaD"),
+// 	}, logr.Discard())
+//
+// 	require.NoError(t, handler(&modules.BatchHandlerCtx{
+// 		Batch: []*userop.UserOperation{
+// 			{
+// 				Sender: common.HexToAddress("0x000000000000000000000000000000000000dEaD"),
+// 			},
+// 		},
+// 	}))
+// }
+//
+// func TestCheckSenderWhitelist_AddressNotWhitelisted(t *testing.T) {
+//
+// 	db, err := badger.Open(badger.DefaultOptions("testdata/test.db").WithInMemory(true))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	// empty whitelist
+// 	handler := CheckSenderWhitelist(db, []common.Address{}, logr.Discard())
+//
+// 	require.NoError(t, handler(&modules.BatchHandlerCtx{
+// 		Batch: []*userop.UserOperation{
+// 			{
+// 				Sender: common.HexToAddress("0x000000000000000000000000000000000000dEaD"),
+// 			},
+// 		},
+// 	}))
+// }
+
 func TestCheckSenderWhitelist(t *testing.T) {
 
-	db, err := badger.Open(badger.DefaultOptions("testdata/test.db").WithInMemory(true))
-	if err != nil {
-		log.Fatal(err)
+	tt := []struct {
+		name                 string
+		whitelistedAddresses []string
+		addressToUseInTx     []string
+		expectedBatchNumber  int
+	}{
+		{
+			name:                 "no whitelisted address",
+			whitelistedAddresses: []string{},
+			addressToUseInTx:     []string{"0x000000000000000000000000000000000000dEaD"},
+			expectedBatchNumber:  1,
+		},
+		{
+			name:                 "whitelisted address and matches",
+			whitelistedAddresses: []string{"0x000000000000000000000000000000000000dEaD"},
+			addressToUseInTx:     []string{"0x000000000000000000000000000000000000dEaD"},
+			expectedBatchNumber:  1,
+		},
+		{
+			name:                 "whitelisted address but does not match",
+			whitelistedAddresses: []string{"0x000000000000000000000000000000000000dEaD"},
+			addressToUseInTx:     []string{"0xdAC17F958D2ee523a2206206994597C13D831ec7"},
+			expectedBatchNumber:  0,
+		},
+		{
+			name:                 "allowed and non allowed addresses",
+			whitelistedAddresses: []string{"0x000000000000000000000000000000000000dEaD", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"},
+			// only 1 address allowed
+			addressToUseInTx:    []string{"0xdAC17F958D2ee523a2206206994597C13D831ec7", "0x000000000000000000000000000000000000dEaD"},
+			expectedBatchNumber: 1,
+		},
+		{
+			name:                 "all whitelisted addresses",
+			whitelistedAddresses: []string{"0x000000000000000000000000000000000000dEaD", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"},
+			addressToUseInTx:     []string{"0x000000000000000000000000000000000000dEaD", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"},
+			expectedBatchNumber:  2,
+		},
 	}
 
-	defer func() {
-		require.NoError(t, db.Close())
-	}()
+	for _, v := range tt {
 
-	handler := CheckSenderWhitelist(db, []common.Address{
-		common.HexToAddress("0x000000000000000000000000000000000000dEaD"),
-	}, logr.Discard())
+		t.Run(v.name, func(t *testing.T) {
+			db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+			require.NoError(t, err)
 
-	require.NoError(t, handler(&modules.BatchHandlerCtx{
-		Batch: []*userop.UserOperation{
-			{
-				Sender: common.HexToAddress("0x000000000000000000000000000000000000dEaD"),
-			},
-		},
-	}))
-}
+			defer func() {
+				require.NoError(t, db.Close())
+			}()
 
-func TestCheckSenderWhitelist_AddressNotWhitelisted(t *testing.T) {
+			whitelistedAddresses := make([]common.Address, len(v.whitelistedAddresses))
 
-	db, err := badger.Open(badger.DefaultOptions("testdata/test.db").WithInMemory(true))
-	if err != nil {
-		log.Fatal(err)
+			for _, addr := range v.whitelistedAddresses {
+				whitelistedAddresses = append(whitelistedAddresses, common.HexToAddress(addr))
+			}
+
+			handler := CheckSenderWhitelist(db, whitelistedAddresses, logr.Discard())
+
+			batch := []*userop.UserOperation{}
+
+			// build multiple user ops
+			for _, addr := range v.addressToUseInTx {
+				batch = append(batch, &userop.UserOperation{
+					Sender: common.HexToAddress(addr),
+				})
+			}
+
+			composedHandler := modules.ComposeBatchHandlerFunc(handler, func(ctx *modules.BatchHandlerCtx) error {
+				// handler to verify the batch has been modified based off the whitelist
+				require.Len(t, ctx.Batch, v.expectedBatchNumber)
+				return nil
+			})
+
+			require.NoError(t, composedHandler(&modules.BatchHandlerCtx{
+				Batch: batch,
+			}))
+
+		})
 	}
-
-	// empty whitelist
-	handler := CheckSenderWhitelist(db, []common.Address{}, logr.Discard())
-
-	require.NoError(t, handler(&modules.BatchHandlerCtx{
-		Batch: []*userop.UserOperation{
-			{
-				Sender: common.HexToAddress("0x000000000000000000000000000000000000dEaD"),
-			},
-		},
-	}))
 }
